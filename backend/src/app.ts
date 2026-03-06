1b753abcbf6b8febbf5a16c97f0106d7331acd9e
AreLory <arena.lorenzo2406@gmail.com>
2026-03-06 15:39:10 +0100

    cors updated

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -34,6 +34,7 @@ app.use(
       "http://localhost:5174",
       "http://localhost:3000",
       "http://127.0.0.1:5173",
+      "https://insolito-panino.onrender.com"
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
